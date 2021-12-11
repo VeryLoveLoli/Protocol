acaa3e04d27f3e8722f4f41d3af6369eb636b2d6
VeryLoveLoli <chuancan@icloud.com>
2021-12-11 17:11:30 +0800

    添加 元件模型协议

---
+++ Sources/Protocol/CellModelProtocol.swift
@@ -0,0 +1,18 @@
+//
+//  CellModelProtocol.swift
+//  
+//
+//  Created by 韦烽传 on 2021/12/11.
+//
+
+import Foundation
+import UIKit
+
+/**
+ 元件模型协议
+ */
+public protocol CellModelProtocol {
+    
+    /// 元件ID
+    var identifier: String? { get set }
+}
